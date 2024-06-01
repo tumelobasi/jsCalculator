a189d312548737e2e16a0c94efa30670349a099e
tumelobasi <83282621+tumelobasi@users.noreply.github.com>
2024-06-01 07:54:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,161 @@
+import React, { useState, useEffect } from "https://cdn.skypack.dev/react@17.0.1";
+import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
+
+const calcData = [
+  { id: "clear", value: "AC" },
+  { id: "divide", value: "/" },
+  { id: "multiply", value: "x" },
+  { id: "seven", value: 7 },
+  { id: "eight", value: 8 },
+  { id: "nine", value: 9 },
+  { id: "subtract", value: "-" },
+  { id: "four", value: 4 },
+  { id: "five", value: 5 },
+  { id: "six", value: 6 },
+  { id: "add", value: "+" },
+  { id: "one", value: 1 },
+  { id: "two", value: 2 },
+  { id: "three", value: 3 },
+  { id: "equals", value: "=" },
+  { id: "zero", value: 0 },
+  { id: "decimal", value: "." },
+];
+
+const operators = ["AC", "/", "x", "+", "-", "="];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const Display = ({ input, output }) => (
+  <div className="output">
+    <span className="result">{output}</span>
+    <span id="display" className="input">{input}</span>
+  </div>
+);
+
+const Key = ({ keyData: { id, value }, handleInput }) => (
+  <button id={id} onClick={() => handleInput(value)}>
+    {value}
+  </button>
+);
+
+const Keyboard = ({ handleInput }) => (
+  <div className="keys">
+    {calcData.map((key) => (
+      <Key key={key.id} keyData={key} handleInput={handleInput} />
+    ))}
+  </div>
+);
+
+const App = () => {
+  const [input, setInput] = useState("0");
+  const [output, setOutput] = useState("");
+  const [calculatorData, setCalculatorData] = useState("");
+
+  const handleSubmit = () => {
+    try {
+      const total = eval(calculatorData.replace(/x/g, "*"));
+      setInput(total);
+      setOutput(`${total} = ${total}`);
+      setCalculatorData(`${total}`);
+    } catch (error) {
+      setOutput("Error");
+    }
+  };
+
+  const handleClear = () => {
+    setInput("0");
+    setCalculatorData("");
+  };
+
+  const handleNumbers = (value) => {
+    if (!calculatorData.length) {
+      setInput(`${value}`);
+      setCalculatorData(`${value}`);
+    } else {
+      if (value === 0 && (calculatorData === "0" || input === "0")) {
+        setCalculatorData(`${calculatorData}`);
+      } else {
+        const lastChar = calculatorData.charAt(calculatorData.length - 1);
+        const isLastCharOperator = lastChar === "*" || operators.includes(lastChar);
+
+        setInput(isLastCharOperator ? `${value}` : `${input}${value}`);
+        setCalculatorData(`${calculatorData}${value}`);
+      }
+    }
+  };
+
+  const dotOperator = () => {
+    const lastChar = calculatorData.charAt(calculatorData.length - 1);
+    if (!calculatorData.length) {
+      setInput("0.");
+      setCalculatorData("0.");
+    } else {
+      if (lastChar === "*" || operators.includes(lastChar)) {
+        setInput("0.");
+        setCalculatorData(`${calculatorData}0.`);
+      } else {
+        setInput(
+          lastChar === "." || input.includes(".") ? `${input}` : `${input}.`
+        );
+        const formattedValue =
+          lastChar === "." || input.includes(".")
+            ? `${calculatorData}`
+            : `${calculatorData}.`;
+        setCalculatorData(formattedValue);
+      }
+    }
+  };
+
+  const handleOperators = (value) => {
+    if (calculatorData.length) {
+      setInput(`${value}`);
+      const beforeLastChar = calculatorData.charAt(calculatorData.length - 2);
+      const beforeLastCharIsOperator = operators.includes(beforeLastChar) || beforeLastChar === "*";
+      const lastChar = calculatorData.charAt(calculatorData.length - 1);
+      const lastCharIsOperator = operators.includes(lastChar) || lastChar === "*";
+      const validOp = value === "x" ? "*" : value;
+
+      if (
+        (lastCharIsOperator && value !== "-") ||
+        (beforeLastCharIsOperator && lastCharIsOperator)
+      ) {
+        if (beforeLastCharIsOperator) {
+          const updatedValue = `${calculatorData.substring(0, calculatorData.length - 2)}${value}`;
+          setCalculatorData(updatedValue);
+        } else {
+          setCalculatorData(`${calculatorData.substring(0, calculatorData.length - 1)}${validOp}`);
+        }
+      } else {
+        setCalculatorData(`${calculatorData}${validOp}`);
+      }
+    }
+  };
+
+  const handleInput = (value) => {
+    if (numbers.includes(value)) {
+      handleNumbers(value);
+    } else if (value === ".") {
+      dotOperator();
+    } else if (operators.includes(value)) {
+      handleOperators(value);
+    } else if (value === "=") {
+      handleSubmit();
+    } else if (value === "AC") {
+      handleClear();
+    }
+  };
+
+  useEffect(() => {
+    setOutput(calculatorData);
+  }, [calculatorData]);
+
+  return (
+    <div className="container">
+      <div className="calculator">
+        <Display input={input} output={output} />
+        <Keyboard handleInput={handleInput} />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
